fe233dbea191b52094c92d80205e157c8a455b52
Ilya <youdeaddark@gmail.com>
2017-03-21 20:41:55 +0500

    Changed visual type

---
+++ src/ru/Deaddark/Main.java
@@ -0,0 +1,18 @@
+package ru.Deaddark;/**
+ * Created by Ilya on 20.03.2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
